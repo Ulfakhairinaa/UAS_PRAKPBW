d3010fb6ca5a9a07400a2e31fc1af9bb80e9ddf3
Ulfa Khairina <ulfa241@mhs.usk.ac.id>
2026-06-02 18:05:25 +0700

    Menambahkan sedikit css

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Kelola Event</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/admin-event.css') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -17,7 +19,7 @@
             <p class="text-muted mb-0">Event untuk kode prodi: {{ session('admin_code') }}</p>
         </div>
 
-        <a href="/admin/events/create" class="btn btn-primary rounded-3">
+        <a href="/admin/events/create" class="btn btn-primary-custom">
             + Tambah Event
         </a>
     </div>
@@ -53,10 +55,14 @@
                             <td>{{ $event->title }}</td>
                             <td>{{ $event->event_date }}</td>
                             <td>{{ $event->location }}</td>
-                            <td>
-                                <span class="badge bg-primary">
-                                    {{ ucfirst($event->status) }}
-                                </span>
+                           <td>
+                                @if($event->status == 'upcoming')
+                                    <span class="badge badge-upcoming">Upcoming</span>
+                                @elseif($event->status == 'ongoing')
+                                    <span class="badge badge-ongoing">Ongoing</span>
+                                @else
+                                    <span class="badge badge-done">Done</span>
+                                @endif
                             </td>
                             <td>
                                 <a href="/admin/events/{{ $event->id }}/edit" class="btn btn-sm btn-warning">
