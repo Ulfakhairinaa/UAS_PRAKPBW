209e1536c6e735ef92604fc3e0952ca24a839c15
Meurahmah <meurah24@mhs.usk.ac.id>
2026-06-02 17:34:28 +0700

    Tambah CSS

--- resources/views/user/events.blade.php
+++ resources/views/user/events.blade.php
@@ -6,6 +6,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/user-event.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body class="bg-light">
